fbcfb34e8dfd14c834d9e196c323401e12695960
Rory Hunter <roryhunter2@gmail.com>
2021-03-01 13:42:36 +0000

    Add support for a Docker init.d directory
    Allow users to customize an Elasticsearch container environment by
    providing the `/docker-entrypoint-init.d` directory in a container and
    putting shell scripts into it. These scripts will be executed before
    Elasticsearch runs.

--- distribution/docker/src/docker/bin/docker-entrypoint.sh
+++ distribution/docker/src/docker/bin/docker-entrypoint.sh
@@ -32,45 +32,81 @@ fi
 # is idempotent.
 source /usr/share/elasticsearch/bin/elasticsearch-env-from-file
 
-if [[ -f bin/elasticsearch-users ]]; then
-  # Check for the ELASTIC_PASSWORD environment variable to set the
-  # bootstrap password for Security.
-  #
-  # This is only required for the first node in a cluster with Security
-  # enabled, but we have no way of knowing which node we are yet. We'll just
-  # honor the variable if it's present.
-  if [[ -n "$ELASTIC_PASSWORD" ]]; then
-    [[ -f /usr/share/elasticsearch/config/elasticsearch.keystore ]] || (elasticsearch-keystore create)
-    if ! (elasticsearch-keystore has-passwd --silent) ; then
-      # keystore is unencrypted
-      if ! (elasticsearch-keystore list | grep -q '^bootstrap.password$'); then
-        (echo "$ELASTIC_PASSWORD" | elasticsearch-keystore add -x 'bootstrap.password')
-      fi
-    else
-      # keystore requires password
-      if ! (echo "$KEYSTORE_PASSWORD" \
-          | elasticsearch-keystore list | grep -q '^bootstrap.password$') ; then
-        COMMANDS="$(printf "%s\n%s" "$KEYSTORE_PASSWORD" "$ELASTIC_PASSWORD")"
-        (echo "$COMMANDS" | elasticsearch-keystore add -x 'bootstrap.password')
+setup_keystore() {
+  if [[ -f bin/elasticsearch-users ]]; then
+    # Check for the ELASTIC_PASSWORD environment variable to set the
+    # bootstrap password for Security.
+    #
+    # This is only required for the first node in a cluster with Security
+    # enabled, but we have no way of knowing which node we are yet. We'll just
+    # honor the variable if it's present.
+    if [[ -n "$ELASTIC_PASSWORD" ]]; then
+      [[ -f /usr/share/elasticsearch/config/elasticsearch.keystore ]] || (elasticsearch-keystore create)
+      if ! (elasticsearch-keystore has-passwd --silent) ; then
+        # keystore is unencrypted
+        if ! (elasticsearch-keystore list | grep -q '^bootstrap.password$'); then
+          (echo "$ELASTIC_PASSWORD" | elasticsearch-keystore add -x 'bootstrap.password')
+        fi
+      else
+        # keystore requires password
+        if ! (echo "$KEYSTORE_PASSWORD" \
+            | elasticsearch-keystore list | grep -q '^bootstrap.password$') ; then
+          COMMANDS="$(printf "%s\n%s" "$KEYSTORE_PASSWORD" "$ELASTIC_PASSWORD")"
+          (echo "$COMMANDS" | elasticsearch-keystore add -x 'bootstrap.password')
+        fi
       fi
     fi
   fi
-fi
+}
 
-if [[ -n "$ES_LOG_STYLE" ]]; then
-  case "$ES_LOG_STYLE" in
-    console)
-      # This is the default. Nothing to do.
-      ;;
-    file)
-      # Overwrite the default config with the stack config
-      mv /usr/share/elasticsearch/config/log4j2.file.properties /usr/share/elasticsearch/config/log4j2.properties
-      ;;
-    *)
-      echo "ERROR: ES_LOG_STYLE set to [$ES_LOG_STYLE]. Expected [console] or [file]" >&2
-      exit 1 ;;
-  esac
-fi
+configure_logging() {
+  if [[ -n "$ES_LOG_STYLE" ]]; then
+    case "$ES_LOG_STYLE" in
+      console)
+        # This is the default. Nothing to do.
+        ;;
+      file)
+        # Overwrite the default config with the stack config
+        mv /usr/share/elasticsearch/config/log4j2.file.properties /usr/share/elasticsearch/config/log4j2.properties
+        ;;
+      *)
+        echo "ERROR: ES_LOG_STYLE set to [$ES_LOG_STYLE]. Expected [console] or [file]" >&2
+        exit 1 ;;
+    esac
+  fi
+}
+
+run_init_scripts() {
+  if [[ -d /docker-entrypoint-initdb.d ]]; then
+    for init_file in /docker-entrypoint-init.d/* ; do
+      if [[ ! -f "$init_file" ]]; then
+        echo "$0: ignoring $init_file"
+        continue
+      fi
+
+      case "$init_file" in
+        *.sh)
+          if [[ -x "$init_file" ]]; then
+            echo "$0: running $init_file"
+            "$init_file"
+          else
+            echo "$0: sourcing $init_file"
+            source "$init_file"
+          fi
+          ;;
+
+        *)
+          echo "$0: ignoring $init_file"
+          ;;
+
+      esac
+    done
+  fi
+}
+
+setup_keystore
+configure_logging
+run_init_scripts
 
 # Signal forwarding and child reaping is handled by `tini`, which is the
 # actual entrypoint of the container
